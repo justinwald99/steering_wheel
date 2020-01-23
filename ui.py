d74c6bf6c1ef0b6e6f5057136e333e7a3baaba8d
jtwald <jtwald@ncsu.edu>
2020-01-22 22:32:51 -0500

    Big progress on screen

--- ui.py
+++ ui.py
@@ -1,15 +1,16 @@
 import sys, pygame
+import random
+from ui_utils import Gauge, GearDisplay
+
 pygame.init()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+print(f'{screen.get_width()} x {screen.get_height()}')
 
-gearPos = "2"
-
-screen = pygame.display.set_mode()
-gearFont = pygame.font.Font('freesansbold.ttf', 128)
-
-gearText = gearFont.render(gearPos, False, (255,255,255))
-gearTextRect = gearText.get_rect()
-
-
+oilPressure = Gauge(screen, (100, 150), "Oil P", 'PSI', 0, 200)
+oilTemperature = Gauge(screen, (220, 150), "Oil T", 'F', 0, 250)
+waterTemperature = Gauge(screen, (400, 150), "Water T", 'F', 0, 220)
+fuelPressure = Gauge(screen, (500, 150), "Fuel P", 'PSI', 0, 500)
+gearPosition = GearDisplay(screen)
 
 while 1:
     for event in pygame.event.get():
@@ -19,16 +20,12 @@ while 1:
             if event.key == pygame.K_ESCAPE:
                 sys.exit()
 
-    screen.fill((0,0,0))
-    screen.blit(gearText, (screen.get_width() / 2, screen.get_height() / 2))
+    screen.fill((255, 255, 255))
+
+    oilPressure.updateGauge(140)
+    oilTemperature.updateGauge(210)
+    waterTemperature.updateGauge(180)
+    fuelPressure.updateGauge(300)
+    gearPosition.updateGear(3)
+
     pygame.display.flip()
-<<<<<<< HEAD
-=======
-    
-def makeGuage(surface, coordinates, label, min, max, ) {
-    GUAGE_WIDTH = 40
-    GUAGE_HEIGHT = 100
-    outline = pygame.Rect(coordinates, (GUAGE_WIDTH, GUAGE_HEIGHT))
-    pygame.draw.rect(surface, coordinates)
-}
->>>>>>> 1cf6711506f11c1ba73ac1425256fb954ad07178
---
+++ ui_utils.py
@@ -0,0 +1,73 @@
+import pygame
+
+class Gauge():
+
+    GAUGE_WIDTH = 40
+    GAUGE_HEIGHT = 200
+    GAUGE_OUTLINE_WIDTH = 5
+    GAUGE_FILL_COLOR = (255, 0, 0)
+    GAUGE_OUTLINE_COLOR = (0, 0, 0)
+    GAUGE_LABEL_COLOR = (0, 0, 0)
+    TEXT_SIZE = 24
+
+    def __init__(self, surface, coordinates, label, unit, valueMin, valueMax):
+        self.surface = surface
+        self.coordinates = coordinates
+        self.label = label
+        self.unit = unit
+        self.min = valueMin
+        self.max = valueMax
+
+        self.value = valueMin 
+
+    def updateGauge(self, value):
+        self.value = value
+        self.draw()
+
+    def draw(self):
+        # Label
+        labelFont = pygame.font.Font('freesansbold.ttf', self.TEXT_SIZE)
+        labelText = labelFont.render(self.label, False, self.GAUGE_LABEL_COLOR)
+        self.surface.blit(
+            labelText, 
+            (self.coordinates[0] + self.GAUGE_WIDTH / 2 - labelText.get_width() / 2, self.coordinates[1] + self.GAUGE_HEIGHT)
+        )
+
+        # Gauge fill
+        valueRange = self.max - self.min
+        gaugePercentage = self.value / valueRange
+        fillHeight = min(self.GAUGE_HEIGHT, round(gaugePercentage * self.GAUGE_HEIGHT))
+        fillCoords = (self.coordinates[0], self.coordinates[1] + self.GAUGE_HEIGHT - fillHeight) 
+        fillRect = pygame.Rect(fillCoords, (self.GAUGE_WIDTH, fillHeight))
+        pygame.draw.rect(self.surface, self.GAUGE_FILL_COLOR, fillRect)
+
+        # Outline
+        outlineRect = pygame.Rect(self.coordinates, (self.GAUGE_WIDTH, self.GAUGE_HEIGHT))
+        pygame.draw.rect(self.surface, self.GAUGE_OUTLINE_COLOR, outlineRect, self.GAUGE_OUTLINE_WIDTH)
+
+        # Exact readout
+        readoutFont = pygame.font.Font('freesansbold.ttf', self.TEXT_SIZE)
+        readoutText = readoutFont.render(f'{self.value} {self.unit}', False, self.GAUGE_LABEL_COLOR)
+        self.surface.blit(
+            readoutText, 
+            (self.coordinates[0] + self.GAUGE_WIDTH / 2 - readoutText.get_width() / 2, self.coordinates[1] - readoutText.get_height())
+        )
+
+class GearDisplay():
+
+    GEAR_COLOR = (0, 0, 0)
+    GEAR_SIZE = 128
+
+    def __init__(self, surface):
+        self.gear = 'N'
+        self.surface = surface
+        
+    def updateGear(self, gear):
+        self.gear = gear
+        self.draw()
+
+    def draw(self):
+        gearFont = pygame.font.Font('freesansbold.ttf', self.GEAR_SIZE)
+        gearText = gearFont.render(str(self.gear), False, self.GEAR_COLOR)
+        self.surface.blit(gearText, (self.surface.get_width() / 2 - gearText.get_width() / 2, self.surface.get_height() / 2 - gearText.get_height() / 2))
+
